768970c7e96fec9cd990862f6eddeee8c5635e63
tukuanchung <tukuanchung@gmail.com>
2021-01-02 13:39:48 +0800

    add count

--- guess.py
+++ guess.py
@@ -1,14 +1,17 @@
 import random
 
 r = random.randint(1,100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('please input a number: ')
 	num = int(num)
 	if num == r:
 		print('you got it!')
+		print('This is ', count ,' times you guessed')
 		break
 	elif num > r:
 		print('more then answer')
 	else num < r:
 		print('lower than anser')
-		
+	print('This is ', count ,' times you guessed')
